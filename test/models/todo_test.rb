73975656349edd805908356f4bd1ac5fdb2e8c96
Dan <dchen12812@gmail.com>
2015-07-12 01:06:51 -0700

    added testing for model

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@todo = Todo.new(todo_item: "Become a cat")
+  end
+
+  test "should be valid" do
+  	assert @todo.valid?
+  end
+
+  test "todo_item should be present" do
+  	@todo.todo_item = "         "
+    assert_not @todo.valid?
+  end
 end
